66022ee2f2ab83cdb1159064c449146dad2824a4
Amadeus Folego <amadeusfolego@gmail.com>
2012-02-12 01:26:35 -0200

    Adds #intersection and #union specs

--- spec/interval_spec.rb
+++ spec/interval_spec.rb
@@ -1,6 +1,57 @@
 require 'borel/interval'
 
 describe Interval do
+  context '#intersection' do
+
+    it "[]^[0,3] = []" do
+      pending
+      (Interval[] ^ Interval[0,3]).should eq Interval[]
+    end
+
+    it "[0,3]^[] = []" do
+      pending
+      (Interval[0,3] ^ Interval[]).should eq Interval[]
+    end
+
+    it "[0,2]^[1] = [1]" do
+      (Interval[0,2] ^ Interval[1]).should eq Interval[1]
+    end
+
+    it "[-infty, infty]^[0,1] = [0,1]" do
+      (Interval[-Infinity, Infinity] ^ Interval[0,1]).should eq Interval[0,1]
+    end
+
+    it "[0,3]^[1,2] = [1,2]" do
+      (Interval[0,3] ^ Interval[1,2]).should eq Interval[1,2]
+    end
+
+    it "[[-2,-1],[1,2]]^[[0,3],[4,5]] = [1,2]" do
+      (Interval[[-2,-1],[1,2]] ^ Interval[[0,3],[4,5]]).should eq Interval[1,2]
+    end
+  end
+
+  context '#union' do
+    it "[0,3]U[1,2] = [0,3]" do
+      (Interval[0,3] | Interval[1,2]).should eq Interval[0,3]
+    end
+
+    it "[1,2]U[0,3] = [0,3]" do
+      (Interval[1,2] | Interval[0,3]).should eq Interval[0,3]
+    end
+
+    it "[1,2]U[1,2] = [1,2]" do
+      (Interval[1,2] | Interval[1,2]).should eq Interval[1,2]
+    end
+
+    it "[1,2]U[2,3] = [1,3]" do
+      (Interval[1,2] | Interval[2,3]).should eq Interval[1,3]
+    end
+
+    it "[1,2]U[3,4] = [[1,2],[3,4]]" do
+      (Interval[1,2] | Interval[3,4]).should eq Interval[[1,2],[3,4]]
+    end
+  end
+
   context '#minus' do
     it "[2,3]-(0,1) = [2,3]" do
       (Interval[2,3] - Interval[0,1]).should eq Interval[2,3]
@@ -15,7 +66,7 @@ describe Interval do
     end
 
     it '[0,3]-(1,2) = [0,1]U[2,3]' do
-      (Interval[0,3] - Interval[1,2]).should eq Interval[0,1] | Interval[2,3]
+      (Interval[0,3] - Interval[1,2]).should eq Interval[[0,1],[2,3]]
     end
 
     it '[0,1]-(0,1) = [0]U[1]' do
