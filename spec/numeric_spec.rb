daae2a2fb58b816d67338c35f86bda1fd88bc74f
Amadeus Folego <amadeusfolego@gmail.com>
2012-02-17 22:45:18 -0200

    Adds Numeric#to_interval specs

---
+++ spec/numeric_spec.rb
@@ -0,0 +1,33 @@
+require 'borel/interval'
+require 'borel/numeric'
+
+describe Numeric do
+  context "#to_interval" do
+
+    it "-1 should be [-1]" do
+      -1.to_interval.should eq Interval[-1]
+    end
+
+    it "0 should be [0]" do
+      0.to_interval.should eq Interval[0]
+    end
+
+    it "1 should be [1]" do
+      1.to_interval.should eq Interval[1]
+    end
+
+    it "-3.23 should be [-3.23]" do
+      -3.23.to_interval.should eq Interval[-3.23]
+    end
+
+    it "1/0 should be [Infinity]" do
+      (1/0.0).to_interval.should eq Interval[Infinity]
+    end
+
+    it "Infinity should be [Infinity]" do
+      (-1/0.0).to_interval.should eq Interval[-Infinity]
+    end
+
+  end
+end
+
