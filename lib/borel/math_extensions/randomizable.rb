33e77d93b142302eb4c4b729f2a3497bd7c7c5fd
Amadeus Folego <amadeusfolego@gmail.com>
2012-02-24 17:48:08 -0200

    Full working randomizable

--- lib/borel/math_extensions/randomizable.rb
+++ lib/borel/math_extensions/randomizable.rb
@@ -1,12 +1,20 @@
-require 'borel'
+require 'borel/math_extensions/interval_arithmetic'
 
 module Borel
   module Randomizable
     def random
-      Random.new.rand(Range.new(*self.extrema))
+      if self.simple?
+        Random.new.rand Range.new *self.extrema
+      elsif self.degenerate?
+        self.extrema.first
+      else
+        total_weight = self.map(&:width).reduce(:+)
+        selected_weight = Random.new.rand 0..total_weight
+        rand_interval = self.find{|c| (selected_weight -= c.width) <= 0}
+        Random.new.rand Range.new *rand_interval.extrema
+      end
     end
 
     Interval.send(:include, self)
   end
 end
-
--- spec/math_extensions/randomizable_spec.rb
+++ spec/math_extensions/randomizable_spec.rb
@@ -11,6 +11,7 @@ describe Borel::Randomizable do
 
   context '#random' do
     let(:interval){ Interval[1,10] }
+    let(:rand_mock) { mock }
 
     it 'should be a point inside the interval' do
       interval.should include interval.random
@@ -18,16 +19,26 @@ describe Borel::Randomizable do
 
     it 'should be a random number' do
       random_number = 3
-      rand_mock = mock
       Random.should_receive(:new).and_return(rand_mock)
       rand_mock.should_receive(:rand).with(1..10).and_return(random_number)
 
       interval.random.should be random_number
     end
 
-    it 'should run on multiple intervals' do
-      pending
+    it 'should run on multiple intervals by weight' do
       interval = Interval[[1,5],[6,8]]
+
+      Random.should_receive(:new).exactly(2).times.and_return(rand_mock)
+      rand_mock.should_receive(:rand).with(0..6).and_return(4)
+      rand_mock.should_receive(:rand).with(1..5).and_return(3)
+
+      interval.random.should be 3
+
+      Random.should_receive(:new).exactly(2).times.and_return(rand_mock)
+      rand_mock.should_receive(:rand).with(0..6).and_return(6)
+      rand_mock.should_receive(:rand).with(6..8).and_return(7)
+
+      interval.random.should be 7
     end
   end
 
