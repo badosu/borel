9e63ad93d3db43f1178de36e1a35ef3bf5d1ac57
Amadeus Folego <amadeusfolego@gmail.com>
2012-02-24 16:39:02 -0200

    Includes self to Interval

--- lib/borel/math_extensions/randomizable.rb
+++ lib/borel/math_extensions/randomizable.rb
@@ -5,7 +5,8 @@ module Borel
     def random
       Random.new.rand(Range.new(*self.extrema))
     end
+
+    Interval.send(:include, self)
   end
 end
 
-Interval.send(:include, Borel::Randomizable)
